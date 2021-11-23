f626dd35da4c32a3839f699024a5a441dc70d379
smaooo <31223968+smaooo@users.noreply.github.com>
2021-11-23 22:46:31 +0000

    Working on adding rules manually in addon

--- __init__.py
+++ __init__.py
@@ -27,8 +27,11 @@ def initiateLSystem(self, context):
             'rule5': {'axiom': 'X', 'angle': 25.7, 'rule': {'F': 'FF', 'X': 'F[+X][-X]FX'}},
             'rule6': {'axiom': 'X', 'angle': 22.5, 'rule': {'F': 'FF', 'X': 'F-[[X]+X]+F[+FX]-X'}}}
     
-    
+
 class OBJECT_OT_add_object(Operator, AddObjectHelper):
+    
+
+    numCusRules = 1
     pcoll = bpy.utils.previews.new()
     iconPath = dirname(bpy.data.filepath) + '\Materials\Icon.png'
     pcoll.load("tree_icon", iconPath, 'IMAGE')
@@ -42,13 +45,16 @@ class OBJECT_OT_add_object(Operator, AddObjectHelper):
                 ('rule3', 'Rule 3', 'Rule 3', icon.icon_id, 3),
                 ('rule2', 'Rule 2', 'Rule 4', icon.icon_id, 4),
                 ('rule5', 'Rule 5', 'Rule 5', icon.icon_id, 5),
-                ('rule6', 'Rule 6', 'Rule 6', icon.icon_id, 6)]
+                ('rule6', 'Rule 6', 'Rule 6', icon.icon_id, 6),
+                ('custom', 'Custom', 'Custom Rule', icon.icon_id, 7)]
+    
     rule: prop.EnumProperty(
         items = ruleItems,
         name = 'Rule')
-        
+
+
     genNum: prop.IntProperty(
-        name='Number of Generations',
+        name='Generations',
         description = 'Number of Generations',
         default = 5,
         min = 1, max = 10)
@@ -57,15 +63,80 @@ class OBJECT_OT_add_object(Operator, AddObjectHelper):
         name = 'Size',
         description = 'Size of the tree', 
         min = 0.1)
+    
+    axiom: prop.StringProperty(
+            name = 'Axiom',
+            description = 'Axiom of the tree',
+            default = 'F',
+            maxlen = 1)
+    angle: prop.FloatProperty(
+        name = 'Angle',
+        description = 'Rotation Angle',
+        default = 0)
+    customVariable1: prop.StringProperty(
+        name = 'Variable',
+        description = 'Variable')   
+    customRule1: prop.StringProperty(
+        name = 'Production Rule',
+        description = 'Producion Rule')
+    customVariable2: prop.StringProperty(
+        name = 'Variable',
+        description = 'Variable')   
+    customRule2: prop.StringProperty(
+        name = 'Production Rule',
+        description = 'Producion Rule')
+    customVariable3: prop.StringProperty(
+        name = 'Variable',
+        description = 'Variable')   
+    customRule3: prop.StringProperty(
+        name = 'Production Rule',
+        description = 'Producion Rule')
+    customVariable3: prop.StringProperty(
+        name = 'Variable',
+        description = 'Variable')   
+    customRule3: prop.StringProperty(
+        name = 'Production Rule',
+        description = 'Producion Rule')
 
+    def draw(self, context):
+        
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False
+        layout.prop(self, 'rule')
+        layout.prop(self, 'genNum')
+        layout.prop(self, 'size')
+        if self.rule == 'custom':
+            
+            row = layout.row()
+            row.prop(self, 'axiom')
+            row.prop(self, 'angle')
+            box = layout.box()
+            row = box.row()
+            for num in range(1,self.numCusRules + 1):
+                print(num)
+                a = ('customVariable' + str(num), 'customRule' + str(num))
+                row.prop(self, a[0]) 
+                row.prop(self, a[1])
+            row = box.row()
+            #row.prop(self, property = 'addRule', icon_only = True, event = True, icon = 'ADD')
+            row.operator(operator = 'add_tree.add_rule', icon = 'ADD')
+            
+
+            
+    
     def execute(self, context):
-
+        
         initiateLSystem(self,context)
         return {'FINISHED'}
 
-
-# Registration
-
+class ADD_TREE_OT_add_rule(Operator, AddObjectHelper):
+    bl_idname = "add_tree.add_rule"
+    bl_label = "Add RUle"
+    bl_options = {'REGISTER', 'UNDO'}
+    def execute(self, context):
+        print(45)
+        return {'FINISHED'}
 def add_object_button(self, context):
     pcoll = preview_collections["main"]
     icon = pcoll['tree_icon']
@@ -82,11 +153,13 @@ def register():
     pcoll.load("tree_icon", iconPath, 'IMAGE')
     preview_collections['main'] = pcoll
     bpy.utils.register_class(OBJECT_OT_add_object)
+    bpy.utils.register_class(ADD_TREE_OT_add_rule)
     bpy.types.VIEW3D_MT_mesh_add.append(add_object_button)
 
 
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_add_object)
+    bpy.utils.unregister_class(ADD_TREE_OT_add_rule)
     bpy.types.VIEW3D_MT_mesh_add.remove(add_object_button)
 
 
